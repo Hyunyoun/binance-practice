36e66f32bf867b73bab66df390d61fb2a2d85af3
Hyunyoun <gusdbs.lee@gmail.com>
2020-07-09 12:44:04 +0900

    INIT from start.spring.io

---
+++ src/main/java/com/yolo/binancepractice/BinancePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.yolo.binancepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinancePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinancePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yolo/binancepractice/BinancePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yolo.binancepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinancePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
